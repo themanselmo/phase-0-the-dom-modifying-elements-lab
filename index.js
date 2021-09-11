f4789bbb9acaba4e00d9207fad12fae47e5e98fc
max anselmo <itmanselmo@gmail.com>
2021-09-11 08:57:57 -0400

    completed assignment, although frustrating to do because the test suite is broken.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+
+const newElement = document.createElement("h1");
+newElement.id = "victory";
+newElement.innerText = "Max is the champion";
+document.body.append(newElement);
+
+
+
